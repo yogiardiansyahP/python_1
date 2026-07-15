aa337247ea9b5295e1d899269088d2a7e6f3701b
unknown <yogiarddev@gmail.com>
2026-07-15 13:01:09 +0700

    update materi

--- data_kategori.py
+++ data_kategori.py
@@ -2,13 +2,106 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import boxcox
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import PowerTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 file = pd.read_csv("dataset.handphone.csv")
 
+file.columns = file.columns.str.strip()
+
+print(file.columns)
 
 sns.countplot(x=file['Brand'])
 plt.show()
 
 sns.barplot(x=file['Brand'],y=file['Ukuran_layar'])
 plt.show()
+
+df_encode = pd.get_dummies(file, columns=["Brand"])
+
+print(df_encode)
+
+ordinal_encoder = OrdinalEncoder()
+
+file["Kapasitas_baterai"] = ordinal_encoder.fit_transform(
+    file[["Kapasitas_baterai"]]
+)
+
+print(file.head())
+
+file["hp_encode"] = file.groupby("Brand")["Harga"].transform("mean")
+print(file[["Brand","Harga"]])
+
+file["hp_encode"]=file["Brand"].map(file["Brand"].value_counts())
+print(file)
+
+data = file["Harga"]
+
+data = np.random.exponential(scale=2,size=1000)
+plt.figure(figsize=(8 , 5))
+sns.histplot(data, kde=True, bins=30)
+plt.title("Distirbusi data skewed")
+
+
+data_log = np.log1p(data)
+sns.histplot(data_log, kde=True, bins=30)
+plt.title("Log Transformasion")
+
+
+data_sqrt = np.sqrt(data)
+
+sns.histplot(data_sqrt, kde=True, bins=30)
+plt.title("Square Root Transformation")
+
+
+data_cbrt = np.cbrt(data)
+sns.histplot(data_cbrt, kde=True, bins=30)
+plt.title("Cube rutetransformation")
+
+
+data_boxcox, lambda_bc = boxcox(data + 1)
+
+sns.histplot(data_boxcox,kde= True, bins=30)
+plt.title(f"box cox transformation(lambda =  {lambda_bc:.2f})")
+
+data = file["Harga"]
+
+pt = PowerTransformer(method="yeo-johnson")
+
+data_yeo = pt.fit_transform(data.to_numpy().reshape(-1, 1))
+
+sns.histplot(data_yeo.flatten(), kde=True, bins=30)
+plt.title("Yeo-Johnson Transformation")
+
+
+fc = file[["Harga", "Ram"]].copy()
+
+fc["Pendapatan_penjualan/ram"] = fc["Harga"] / fc["Ram"]
+
+print(fc)
+
+fc["Log_pendapatan"]= np.log(fc["Ram"])
+print(fc)
+
+df = pd.DataFrame({
+    "Spek_hp": pd.date_range(start="2025-01-01", periods=5, freq="D")
+})
+
+
+df["Memori_internal"] = df["Spek_hp"].dt.unit
+df["Ukuran_layar"] = df["Spek_hp"].dt.unit
+df["Resolusi_kamera"] = df["Spek_hp"].dt.unit
+
+print(df)
+
+text_data = ["saya suka belajar machine learning",
+             "machine learning sangat menarik",
+             "saya ingin  memahami  lebih dalam  tentang machine learning"]
+
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(text_data)
+df_tfidf = pd.DataFrame(tfidf_matrix.toarray(),columns=vectorizer.get_feature_names_out())
+print(df_tfidf)
